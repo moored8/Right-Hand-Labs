b4a4d6aa71869ab8261af69add7ad6bf889a72a3
moored8 <moored8@gmail.com>
2026-08-28 11:03:45 -0600

    Update AppShell.tsx

--- app/AppShell.tsx
+++ app/AppShell.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { BookingProvider } from './context/BookingContext';
+import ChatBot from './components/ChatBot';
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
-  return <BookingProvider>{children}</BookingProvider>;
+  return <BookingProvider>{children}<ChatBot /></BookingProvider>;
 }
