ac115398dcaef07eb50e729156157e0d7431232a
moored8 <moored8@gmail.com>
2026-08-28 11:02:01 -0600

    Update ChatBot.tsx

--- app/components/ChatBot.tsx
+++ app/components/ChatBot.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function ChatBot() {
   useEffect(() => {
     // Check if script is already loaded
-    if (document.querySelector('script[data-widget-id="696e48d9b7a8e43accdc7f4c"]')) {
+    if (document.querySelector('script[data-widget-id="6a91b4fb3cbef685c57e6bde"]')) {      return;
       return;
     }
 
@@ -13,14 +13,14 @@ export default function ChatBot() {
     const script = document.createElement('script');
     script.src = 'https://widgets.leadconnectorhq.com/loader.js';
     script.setAttribute('data-resources-url', 'https://widgets.leadconnectorhq.com/chat-widget/loader.js');
-    script.setAttribute('data-widget-id', '696e48d9b7a8e43accdc7f4c');
+    script.setAttribute('data-widget-id', '6a91b4fb3cbef685c57e6bde');
     script.async = true;
     
     document.body.appendChild(script);
 
     // Cleanup function
     return () => {
-      const existingScript = document.querySelector('script[data-widget-id="696e48d9b7a8e43accdc7f4c"]');
+      const existingScript = document.querySelector('script[data-widget-id="6a91b4fb3cbef685c57e6bde"]');
       if (existingScript) {
         existingScript.remove();
       }
